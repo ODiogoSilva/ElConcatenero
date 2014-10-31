c4d7ebe67813e2fc21ef5096291248786ffc39a3
Diogo Silva <o.diogosilva@gmail.com>
2014-10-31 23:54:20 +0000

    Fixed numerous issues

--- ElConcatenero.py
+++ ElConcatenero.py
@@ -33,6 +33,7 @@ parser.add_argument("-if",dest="InputFormat",default="fasta",choices=["fasta","n
 parser.add_argument("-of",dest="OutputFormat",default="nexus",choices=["nexus","phylip","fasta","ima2"],help="Format of the ouput file (default is '%(default)s')")
 parser.add_argument("-c",dest="Conversion",action="store_const",const=True,help="Used for convertion of the input files passed as arguments with the -in option. This flag precludes the usage of the -o option, as the output file name is automatically generated based on the input file name.")
 parser.add_argument("-o",dest="outfile",help="Name of the output file")
+parser.add_argument("-z",dest="zorro",action="store_const",const=True,help="Use this option if you wish to concatenate auxiliary Zorro files associated with each alignment. Note that the auxiliary files must have the same prefix of the alignment file, with the addition of '_zorro.out'")
 parser.add_argument("-in",dest="infile",nargs="+",required=True,help="Provide the input file name. If multiple files are provided, plase separated the names with spaces")
 arg = parser.parse_args()
 
@@ -44,8 +45,8 @@ seq_space_phy = 26
 seq_space_ima2 = 10 # Required by IMa2! Changing this value will most likely break the input file 
 
 # Cut the taxa names by the following character:
-cut_space_nex = 8
-cut_space_phy = 20
+cut_space_nex = 20
+cut_space_phy = 50
 cut_space_ima2 = 8
 
 # If you wish to include a whitespace (tab) between loci, set the variable to "yes", ortherwise "no".
@@ -53,20 +54,28 @@ tab_delimited_loci = "no"
 
 ##### MAIN FUNCTIONS ######
 
+def remove_illegal (name):
+	""" Function that removes illegal characters from taxa names """
+	illegal_chars = [":",",",")","(",";","[","]","'"]
+	clean_name = "".join([char for char in name if char not in illegal_chars])
+	return clean_name
+
 def dataset_creator(infile_list):
 	initial_storage,taxa_order = ElParsito.Taxa_gather(arg.InputFormat,infile_list)
-	storage,part_list,sizes = ElParsito.Elparsito(arg.InputFormat,initial_storage,infile_list,arg.OutputFormat,tab_delimited_loci)
-	return storage,part_list,sizes,taxa_order
+	storage,part_list,sizes,zorro_list = ElParsito.Elparsito(arg.InputFormat,initial_storage,infile_list,arg.OutputFormat,tab_delimited_loci,arg.zorro)
+	return storage,part_list,sizes,taxa_order,zorro_list
 	
-def output_creator(output_format,storage,part_list,sizes,taxa_order,outfile):
+def output_creator(output_format,storage,part_list,sizes,taxa_order,zorro,outfile):
 	if output_format == "ima2":
 		out_file = open(outfile+".txt","w")
-		info = input("Please provide (separated by semicolon): number of populations, name of populations, population tree:\nExample: 3;pop1 pop2 pop3; ((0,1):2\n>>> ")
+		info = input("Please provide (separated by semicolon): number of populations, "
+					"name of populations, population tree:\nExample: 3;pop1 pop2 pop3; "
+					"((0,1):2\n>>> ")
 		# Collecting information about number of populations, population names and population tree
 		npops,pop_names,poptree = info.split(";")
 		out_file.write("Input file for IMa2 using the following dataset(s): "+", ".join(arg.infile)+"\n"+str(npops).strip()+"\n"+pop_names.strip()+"\n"+poptree.strip()+"\n"+str(len(arg.infile))+"\n")
 		for file_i in arg.infile:
-			storage,part_list,sizes,taxa_order = dataset_creator(file_i.split())
+			storage,part_list,sizes,taxa_order, zorro = dataset_creator(file_i.split())
 			locus_info = input("Please provide (separated by semicolon): Sample size for each population in this locus ("+file_i+"), Model of evolution, Mutation rate (if available) and ploidy:\nExample:13 21 32; I; 0.00000003; 1\n>>>")
 			sample_size,model,rate,ploidy = locus_info.split(";")
 			out_file.write(file_i.split(".")[0]+" "+sample_size+" "+str(sizes-1)+" "+model.strip()+" "+ploidy.strip()+" "+rate.strip()+"\n")
@@ -76,12 +85,14 @@ def output_creator(output_format,storage,part_list,sizes,taxa_order,outfile):
 		out_file = open(outfile+".phy","w")
 		out_file.write(str(len(storage))+" "+str(sizes-1)+"\n")
 		for key in taxa_order:
+			clean_key = remove_illegal(key) # Removes illegal characters from the taxon name
 			if arg.Conversion == None:
-				out_file.write(key.replace(" ","_")[:cut_space_phy].ljust(seq_space_phy)+" "+storage[key]+"\n")
-				part_file = open(outfile.split(".")[0]+"_part.File","w")
+				out_file.write(clean_key.replace(" ","_")[:cut_space_phy].ljust(seq_space_phy)+" "+storage[key]+"\n")
+				part_file = open(outfile.split(".")[0]+"_part.File","a")
 				part_file.write("".join(part_list))
 			else:
-				out_file.write(key.replace(" ","_").ljust(seq_space_phy)+" "+storage[key]+"\n")
+				out_file.write(clean_key.replace(" ","_")[:cut_space_phy].ljust(seq_space_phy)+" "+storage[key]+"\n")
+
 	elif output_format == "nexus":
 		out_file = open(outfile+".nex","w")
 		if arg.Conversion:
@@ -96,18 +107,25 @@ def output_creator(output_format,storage,part_list,sizes,taxa_order,outfile):
 		out_file = open(outfile+".fas","w")
 		for key in taxa_order:
 			out_file.write(">"+key.replace(" ","_")+"\n"+storage[key]+"\n")
+	# Zorro implementation
+	if zorro != []:
+		zorro_outfile = open(outfile+"_zorro.out","w")
+		for i in zorro:
+			zorro_outfile.write("%s\n" % (i))
 				
 ##### EXECUTION ######
 				
-if arg.OutputFormat == "ima2":
-	output_creator (arg.OutputFormat)
-elif arg.Conversion:
+if arg.Conversion:
 	for file_i in arg.infile:
-		storage,part_list,sizes,taxa_order = dataset_creator(file_i.split())
-		output_creator(arg.OutputFormat,storage,part_list,sizes,taxa_order,file_i.split(".")[0])
+		if arg.InputFormat == arg.OutputFormat:
+			output_code = file_i.split(".")[0]+"_2"
+		else:
+			output_code = file_i.split(".")[0]
+		storage,part_list,sizes,taxa_order,zorro = dataset_creator(file_i.split())
+		output_creator(arg.OutputFormat,storage,part_list,sizes,taxa_order,zorro,output_code)
 else:
 	if arg.outfile == None:
 		print ("\nOooops!\n>>>If you wish to concatenate, please provide an output file name with the -o option\n>>>If you wish to convert multiple sequences, please use the -c flag\n")
 		raise SystemExit
-	storage,part_list,sizes,taxa_order = dataset_creator(arg.infile)
-	output_creator(arg.OutputFormat,storage,part_list,sizes,taxa_order,arg.outfile)
+	storage,part_list,sizes,taxa_order,zorro = dataset_creator(arg.infile)
+	output_creator(arg.OutputFormat, storage, part_list, sizes, taxa_order, zorro, arg.outfile)
--- ElParsito.py
+++ ElParsito.py
@@ -20,8 +20,8 @@
 #  along with this program; if not,  If not, see <http://www.gnu.org/licenses/>.
 
 import re
-import subprocess
 import sys
+import subprocess
 
 def loading (current_state,size,prefix,width,suffix):
 	""" Function that prints the loading progress of the script """
@@ -43,6 +43,14 @@ def size_check(dic,i_file):
 		print ("\n Unequal sequence lenght detected in %s for the following taxa" % i_file)
 		raise SystemExit
 
+
+def zorro2rax (zorro_infile, zorro_storage):
+	""" Function that converts the floating point numbers contained in the original zorro output files into intergers that can be interpreted by RAxML """
+	for i in zorro_infile:
+		number = float(i.strip())
+		int_number = round(number)
+		zorro_storage.append(int_number)
+	return zorro_storage
 
 def Taxa_gather (Elformat,infile_list):
 	""" Function to gather the taxa names of all input files as keys of a dictionary. Must be performed before the ElParsito function in order to complete the dictionary keys over all input files. Returns a dictionary with the complete set of keys and empty values, and a list of taxa to maintain taxa order in subsequent functions """
@@ -78,15 +86,22 @@ def Taxa_gather (Elformat,infile_list):
 						taxa_order.append(line_s[0])
 	return storage,taxa_order
 
-def Elparsito(Elformat, storage, infile_list, outputFormat,tab_delimited_loci="no"):
+def Elparsito(Elformat, storage, infile_list, outputFormat,tab_delimited_loci="no", zorro=None):
 	""" Function to populate a dictionary with taxa names as keys and their corresponding sequence as values. Returns a complete dictionary, a list of partitions and the total sequence size. """
 	""" The new argument 'tab_delimited_loci' can be set to True when the user wants to separate loci with whitespace (Tab is used here). This is usefull for some programs such as Arlequin """
 	part_list,sizes,cur_file = [],1,0
+	zorro_storage = []
 	for file_i in infile_list:
-		loading (cur_file, len(infile_list), "Processing alignments", 50, "Processing file %s" % (file_i,))
+		counter = 0
+		#loading (cur_file, len(infile_list), "Processing alignments", 50, "Processing file %s" % (file_i,))
 		temp_storage, x = {}, 1
 		file_r = open(file_i,"r")
-		if "phylip" in Elformat:
+		### Zorro implementation
+		if zorro == True:
+			file_prefix = file_i.split(".")[0]
+			zorro_file = open(file_prefix+"_zorro.out")
+			zorro_storage = zorro2rax(zorro_file,zorro_storage)
+		if "phylip" in Elformat and counter == 0:
 			for line in file_r:
 				## Collects information on the number of taxa and locus size that are found on the first line of the file ##
 				if x == 1:
@@ -125,9 +140,12 @@ def Elparsito(Elformat, storage, infile_list, outputFormat,tab_delimited_loci="n
 						storage[key] += "\t"+"n"*int(seq_size)
 					elif tab_delimited_loci == "yes" and storage[key] == "":
 						storage[key] += "n"*int(seq_size)
+			
+			counter = 1
+			### Zorro implementation for phylip input format
 					
 					
-		elif "fasta" in Elformat:
+		elif "fasta" in Elformat and counter == 0:
 			for line in file_r:
 				## When lines start with the character ">" the name of the current taxon is associated with the "id_name" variable ##
 				if line.startswith(">") and line.strip() != "" and tab_delimited_loci == "no":
@@ -146,6 +164,7 @@ def Elparsito(Elformat, storage, infile_list, outputFormat,tab_delimited_loci="n
 					temp_storage[id_name] += line.strip()
 			for i in temp_storage.values():
 				seq_size = len(i)
+			
 				
 			## Creates lists for phylip and nexus output formats containing the the size, range and ID of the current partition ##
 			if "phylip" in outputFormat and len(infile_list) > 1:
@@ -163,9 +182,10 @@ def Elparsito(Elformat, storage, infile_list, outputFormat,tab_delimited_loci="n
 						storage[key] += "\t"+"n"*int(seq_size)
 					elif tab_delimited_loci == "yes" and storage[key] == "":
 						storage[key] += "n"*int(seq_size)
+			
+			counter = 1		
 					
-					
-		elif "nexus" in Elformat:
+		elif "nexus" in Elformat and counter == 0:
 			nchar = "nchar=[0-9]*[0-9]"
 			nchar_regex = re.compile(nchar)
 			temp_storage = {}
@@ -199,7 +219,8 @@ def Elparsito(Elformat, storage, infile_list, outputFormat,tab_delimited_loci="n
 					storage[key] += "n"*int(seq_size)+"\t"
 				elif storage[key] in temp_storage.keys() and tab_delimited_loci == "yes":
 					storage[key] += "\t"
+			counter = 1
 		cur_file += 1
 		file_r.close()
 		#size_check(temp_storage,file_i)
-	return storage, part_list, sizes
+	return storage, part_list, sizes, zorro_storage
